eb1a52cdaf1da471aa1db2eb92f762d104040d81
maximpham <famfanych@gmail.com>
2024-02-27 17:35:49 +0100

    Revert "Revert "Homework #6""
    This reverts commit e330f9aa242cf16d81946161a7a1ad7b6ed6a5dc.

--- src/main/java/ua/fam/homeworks/CounterStrike.java
+++ src/main/java/ua/fam/homeworks/CounterStrike.java
@@ -1,63 +1,38 @@
 package ua.fam.homeworks;
 
-import java.util.Scanner;
-
-import java.util.Scanner;
-
 public class CounterStrike {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        public static void main(String[] args) {
 
-        System.out.print("Enter the first team: ");
-        String team1_name = scanner.nextLine();
+            String teamName = "Team Right";
 
-        System.out.print("Kills of 1st player: ");
-        int team1_score1 = scanner.nextInt();
-        System.out.print("Kills of 2nd player: ");
-        int team1_score2 = scanner.nextInt();
-        System.out.print("Kills of 3rd player: ");
-        int team1_score3 = scanner.nextInt();
-        System.out.print("Kills of 4th player: ");
-        int team1_score4 = scanner.nextInt();
-        System.out.print("Kills of 5th player: ");
-        int team1_score5 = scanner.nextInt();
-        double avg_team1 = (double) (team1_score1 + team1_score2 + team1_score3 + team1_score4
-                + team1_score5) / 5;
+            int playerGreen= 12;
+            int playerRed = 5;
+            int playerBlue = 8;
+            int playerWhite = 3;
+            int playerGrey = 10;
 
-        scanner.nextLine();
+            String teamName2 = "Team Left";
 
-        System.out.print("Enter the second team: ");
-        String team2_name = scanner.nextLine();
+            int playerBlack = 20;
+            int playerPurple = 14;
+            int playerOrange = 1;
+            int playerPink = 5;
+            int playerYellow = 2;
 
-        System.out.print("Kills of 1st player: ");
-        int team2_score1 = scanner.nextInt();
-        System.out.print("Kills of 2nd player: ");
-        int team2_score2 = scanner.nextInt();
-        System.out.print("Kills of 3rd player: ");
-        int team2_score3 = scanner.nextInt();
-        System.out.print("Kills of of 4th player: ");
-        int team2_score4 = scanner.nextInt();
-        System.out.print("Kills of 5th player: ");
-        int team2_score5 = scanner.nextInt();
-        double avg_team2 = (double) (team2_score1 + team2_score2 + team2_score3 + team2_score4
-                + team2_score5) / 5;
+            int pointsTeamRight = (playerGreen + playerRed + playerBlue
+                    + playerWhite + playerGrey) /5;
+            int pointsTeamLeft = (playerBlack + playerPurple + playerOrange +
+                    playerPink + playerYellow) /5;
 
-        String winner;
-        double result;
-        if (avg_team1 > avg_team2) {
-            winner = team1_name;
-            result = avg_team1;
-        } else if (avg_team2 > avg_team1) {
-            winner = team2_name;
-            result = avg_team2;
-        } else {
-            winner = "Draw";
-            result = avg_team1;
+            if (pointsTeamRight > pointsTeamLeft){
+
+                System.out.println("Winner is: " + teamName + " with "
+                        + pointsTeamRight + " points");
+            } else {
+                System.out.println("Winner is: " + teamName2 + " with "
+                        + pointsTeamLeft + " points");
+            }
         }
-
-        System.out.println("The Winner is:  " + winner + " with " + result + " points.");
     }
-}
-
 
