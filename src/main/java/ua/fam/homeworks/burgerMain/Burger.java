384f9d1249b53ea668a73f29e19cd9e8047d3cde
maximpham <famfanych@gmail.com>
2024-03-19 17:15:24 +0100

    Revert "Homework #13"

--- src/main/java/ua/fam/homeworks/burgerMain/Burger.java
+++ src/main/java/ua/fam/homeworks/burgerMain/Burger.java
@@ -1,29 +1,36 @@
 package ua.fam.homeworks.burgerMain;
 
-class Burger {
+public class Burger {
     String bun;
     String meat;
     String cheese;
     String veggies;
-    boolean mayo;
+    String mayo;
 
-    public Burger(String bun, String meat, String cheese, String veggies, boolean mayo) {
+    public Burger(String bun, String meat, String cheese, String veggies, String mayo) {
         this.bun = bun;
         this.meat = meat;
         this.cheese = cheese;
         this.veggies = veggies;
         this.mayo = mayo;
+        displayIngredients();
+    }
 
-        System.out.println("Бургер складається з:");
-        System.out.println("Булочка: " + this.bun);
-        System.out.println("М'ясо: " + this.meat);
-        System.out.println("Сир: " + this.cheese);
-        System.out.println("Зелень: " + this.veggies);
-        if (this.mayo) {
-            System.out.println("Майонез: Є");
-        } else {
-            System.out.println("Майонез: Немає");
-        }
+    public Burger(String bun, String meat, String cheese, String veggies) {
+        this(bun, meat, cheese, veggies, "без майонезу");
+    }
+
+    public Burger(String bun, String meat, String cheese, String veggies, boolean doubleMeat) {
+        this(bun, doubleMeat ? meat + ", подвійне м'ясо" : meat, cheese, veggies);
+    }
+
+    private void displayIngredients() {
+        System.out.println("Склад бургера:");
+        System.out.println("Булочка: " + bun);
+        System.out.println("М'ясо: " + meat);
+        System.out.println("Сир: " + cheese);
+        System.out.println("Зелень: " + veggies);
+        System.out.println("Майонез: " + mayo);
         System.out.println();
     }
 }
--- src/main/java/ua/fam/homeworks/burgerMain/BurgerMain.java
+++ src/main/java/ua/fam/homeworks/burgerMain/BurgerMain.java
@@ -2,12 +2,10 @@ package ua.fam.homeworks.burgerMain;
 
 public class BurgerMain {
     public static void main(String[] args) {
-        System.out.println("Створюємо бургери:");
-        System.out.println("===================");
-
-        Burger regularBurger = new Burger("Картопляна", "Яловичина", "Чеддер", "Помідор", true);
-        Burger dietBurger = new Burger("Пшенична", "Курка", "Бринза", "Огірок", false);
-        Burger doubleMeatBurger = new Burger("Ржана", "Свинина", "Моцарелла", "Цибуля", true);
+        Burger regularBurger = new Burger("Булка з насінням", "Рибне філе", "Сир Чеддер", "Помідор", "Майонез");
+        Burger dietBurger = new Burger("Булка без глютену", "Куряче філе", "Сир моцарелла", "Листя салату");
+        Burger doubleMeatBurger = new Burger("Біг Мак", "Яловичина", "Сир Чеддер", "Цибуля", false);
     }
 }
+
 
